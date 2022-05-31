64347007eb9ca5cfd7ec796213d050e96ff1c1d1
Jaspinder Singh <jaspinder@Jaspinders-MacBook-Pro.local>
2022-05-31 15:45:00 -0700

    changes for search added and basic edit functionality

--- TheUltimateDemo/src/app/app.module.ts
+++ TheUltimateDemo/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { EditComponent } from './edit/edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent
+    SearchComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- TheUltimateDemo/src/app/search/search.component.ts
+++ TheUltimateDemo/src/app/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Person, SearchService } from '../shared';
+
 
 @Component({
   selector: 'app-search',
@@ -6,14 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  query: string;
+  searchResults: Array<Person>;
 
-  constructor() { }
+  constructor(private searchService: SearchService) { }
 
   ngOnInit(): void {
   }
 
   search() {
-    
+    this.searchService.search(this.query).subscribe(
+      (data: any) => { this.searchResults = data; },
+      error => console.log(error)
+    );
   }
 
 }
